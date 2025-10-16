689c7979f6a758ef702e53f9780e1c5aa2fc4ef5
Lídia Gonçalves <90067316+liatsuki@users.noreply.github.com>
2025-10-16 20:07:19 +0100

    161025_2007

---
+++ A1.3_Exercicios1/10_trocos.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double montante;
+    int totalCentimos, i, quantidade, moedas;
+    char nomes;
+
+    printf("Introduza um montante em euros, podendo ter centimos: ");
+    scanf("%f", &montante);
+}
--- A1.3_Exercicios1/9_euler.c
+++ A1.3_Exercicios1/9_euler.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    
+    int K, i=0;
+    double resultado=0.0, factorial=1.0;
+
+    scanf("%d", &K);
+
+    if(K<0) {
+        printf("Erro: K deve ser >= 0.\n");
+    } else {
+        while(i<=K) {
+            if(i==0){
+                resultado+=1.0/factorial;
+                i++;
+            } else if(i>=1) {
+                factorial*=i;
+                resultado+=1.0/factorial;
+                i++;
+            }
+        }
+        printf("Resultado: %.16g", resultado);
+    }
 }
